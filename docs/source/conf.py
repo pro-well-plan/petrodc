0d8adc6d1abda036d14d8beaf1d29d1927f6ea48
jcamiloangarita <camiloang94@gmail.com>
2020-11-10 12:40:23 +0100

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,11 +45,11 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-#autodoc_member_order = "bysource"
+autodoc_member_order = "bysource"
 
-#source_suffix = ".rst"
+source_suffix = ".rst"
 
-#master_doc = "index"
+master_doc = "index"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
